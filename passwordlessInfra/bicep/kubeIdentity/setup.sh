6263eea5081e666cd1bbeb076616b9a6e977a21b
DrBushyTop <aakkoset@gmail.com>
2024-11-10 23:47:08 +0200

    should maybe work

--- passwordlessInfra/bicep/kubeIdentity/setup.sh
+++ passwordlessInfra/bicep/kubeIdentity/setup.sh
@@ -57,9 +57,8 @@ sudo chmod +x /usr/local/bin/kubectl
 
 # Install Minikube
 echo "Installing Minikube..."
-curl -Lo minikube https://storage.googleapis.com/minikube/releases/latest/minikube-linux-amd64
-sudo install minikube /usr/local/bin/
-rm minikube
+curl -LO https://storage.googleapis.com/minikube/releases/latest/minikube-linux-amd64
+sudo install minikube-linux-amd64 /usr/local/bin/minikube && rm minikube-linux-amd64
 
 # Generate RSA keys
 echo "Generating RSA keys..."
@@ -75,11 +74,11 @@ curl -sL https://aka.ms/InstallAzureCLIDeb | sudo bash
 echo "Setting Azure environment variables..."
 export AZURE_STORAGE_ACCOUNT="phcloudbrewoidc"
 export AZURE_STORAGE_CONTAINER="oidccontainer"
-export AZURE_TENANT_ID="your_azure_tenant_id"
-export AZURE_SUBSCRIPTION_ID="your_subscription_id"
-export AAD_APPLICATION_ID="your_aad_application_id"
+export AZURE_TENANT_ID="7135bcf1-5a12-4e82-ad41-c263afa243e8"
+export AZURE_SUBSCRIPTION_ID="ede0939c-80c4-4dfe-bf3d-84521f3f6d1f"
+export AAD_APPLICATION_ID="3f308513-c3f1-453c-9bfe-45d455762a8b"
 export RESOURCE_GROUP_NAME="oidc-resource-group"
-export LOCATION="swedencentral" 
+export LOCATION="swedencentral"
 
 # Login to Azure
 echo "Logging into Azure..."
@@ -149,7 +148,7 @@ sudo mv azwi /usr/local/bin/
 
 # Generate JWKS document
 echo "Generating JWKS document..."
-azwi jwks --public-keys sa.pub --output-file jwks.json
+azwi jwks --public-keys /home/$USER/sa.pub --output-file jwks.json
 
 # Upload JWKS document
 echo "Uploading JWKS document to Azure Storage..."
@@ -166,12 +165,22 @@ echo "Verifying JWKS document..."
 curl -s "https://${AZURE_STORAGE_ACCOUNT}.blob.core.windows.net/${AZURE_STORAGE_CONTAINER}/openid/v1/jwks"
 
 # Start Minikube with required configurations
-echo "Starting Minikube with required configurations..."
+echo "Starting Minikube to set up required configurations..."
 sudo usermod -aG docker $USER && newgrp docker
-minikube start --extra-config=apiserver.service-account-issuer="https://${AZURE_STORAGE_ACCOUNT}.blob.core.windows.net/${AZURE_STORAGE_CONTAINER}/" \
-               --extra-config=apiserver.service-account-signing-key-file="/home/$USER/sa.key" \
-               --extra-config=apiserver.service-account-key-file="/home/$USER/sa.pub" \
-               --extra-config=controller-manager.service-account-private-key-file="/home/$USER/sa.key"
+
+minikube start
+minikube cp /home/$USER/sa.key /var/lib/minikube/certs/sa.key
+minikube cp /home/$USER/sa.pub /var/lib/minikube/certs/sa.pub
+minikube stop
+
+
+echo "Starting Minikube with new config..."
+minikube start \
+  --extra-config=apiserver.service-account-issuer="https://${AZURE_STORAGE_ACCOUNT}.blob.core.windows.net/${AZURE_STORAGE_CONTAINER}/" \
+  --extra-config=apiserver.service-account-signing-key-file="/var/lib/minikube/certs/sa.key" \
+  --extra-config=apiserver.service-account-key-file="/var/lib/minikube/certs/sa.pub" \
+  --extra-config=controller-manager.service-account-private-key-file="/var/lib/minikube/certs/sa.key"
+
 
 # Install Helm
 echo "Installing Helm..."
@@ -198,11 +207,12 @@ export SERVICE_ACCOUNT_NAME="phcloudbrewapp"
 
 # Use azwi to create the Kubernetes Service Account and link it to the Azure AD application
 echo "Linking Kubernetes Service Account with Azure AD application using azwi..."
-azwi serviceaccount add \
+kubectl create namespace $SERVICE_ACCOUNT_NAMESPACE
+
+azwi sa create phase service-account\
   --service-account-namespace $SERVICE_ACCOUNT_NAMESPACE \
   --service-account-name $SERVICE_ACCOUNT_NAME \
-  --aad-application-id $AAD_APPLICATION_ID \
-  --tenant-id $AZURE_TENANT_ID
+  --aad-application-client-id $AAD_APPLICATION_ID 
 
 echo "Service Account creation complete!"
 
